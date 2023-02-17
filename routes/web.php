0689b957c520673b83f3920d83f3ff1ecdb2d095
Sharryy <ibneadam388@gmail.com>
2023-02-18 04:12:43 +0500

    docs: add swagger docs

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Health-check. API is working";
 });
 
 Route::view('/docs', 'swagger');
